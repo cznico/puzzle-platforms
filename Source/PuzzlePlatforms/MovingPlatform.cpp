f499497e71ac0fd79ce26d85cef8c50500f33226
Petr Holec <petr.holec86@gmail.com>
2019-06-01 16:52:50 +0200

    Server replication + console test

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay() {
+	Super::BeginPlay();
+	if (HasAuthority()) {
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) {
+		UE_LOG(LogTemp, Warning, TEXT("Platform ticks"))
+			auto location = GetActorLocation();
+		location += FVector(MovementSpeed * DeltaTime, 0, 0);
+		SetActorLocation(location);
+	}
+	
+}
