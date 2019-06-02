0c3b2de859f6a38ad5404724ba5d70a375abb91f
Petr Holec <petr.holec86@gmail.com>
2019-06-02 20:03:57 +0200

    New class for platform trigger added

--- client.bat
+++ client.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\Epic Games\UE_4.19\Engine\Binaries\Win64\UE4Editor.exe" "c:\Dev\_unreal\PuzzlePlatforms\PuzzlePlatforms.uproject" 192.168.88.253:7777 -game  -log
+"c:\Program Files\Epic Games\UE_4.19\Engine\Binaries\Win64\UE4Editor.exe" "c:\Dev\_unreal\PuzzlePlatforms\PuzzlePlatforms.uproject" 25.63.248.103:7777 -game -log
